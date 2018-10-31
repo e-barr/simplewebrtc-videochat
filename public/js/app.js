efa1d7cfef3f6e10f6ab8fcb23472f383ea56290
e-barr <31676765+e-barr@users.noreply.github.com>
2018-10-31 17:20:27 -0500

    add access to local camera

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,42 @@
 window.addEventListener('load', () => {
-  //Put all client-side code here
-})
+  //Chat platform
+  const chatTemplate = Handlebars.compile($('#chat-template').html());
+  const chatContentTemplate = Handlebars.compile($('#chat-content-template').html());
+  const chatEl = $('#chat');
+  const formEl = $('.form');
+  const messages = [];
+  let username;
+
+  //Local Video
+  const localImageEl = $('#local-image');
+  const localVideoEl = $('#local-video');
+
+  //Remote Video
+  const remoteVideoTemplate = Handlebars.compile($('#remote-video-template').html());
+  const remoteVideoEl = $('#remote-videos')
+  let remoteVideosCount = 0;
+
+  //Add validation rules to Create/Join Room Form
+  formEl.form({
+    fields: {
+      roomName: 'empty',
+      username: 'empty',
+    },
+  });
+
+  //create our WebRTC connection
+  const webrtc = new SimpleWebRTC({
+    //the id/element dom element that will hold local video
+    localVideoEl: 'local-video',
+    //the id/element dom element that will hold remote videos
+    remoteVideosEl: 'remote-videos',
+    //immediately ask for camera access
+    autoRequestMedia: true,
+  });
+
+  //we have access to local camera
+  webrtc.on('localStream', () => {
+    localImageEl.hide();
+    localVideoEl.show();
+  })
+});
